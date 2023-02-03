b352cd23e84f8a3d7bd614e79fd74c2b5273ff0c
Yuliya Sukhodolskaya <ylsukhodolskaya@yandex.ru>
2023-02-03 20:42:07 +0300

    hotfix: попап закрывается принажатии на ссылки, ссылки стилизованы

--- src/components/Main/NavTab/NavTab.jsx
+++ src/components/Main/NavTab/NavTab.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import './NavTab.css';
 import account_logo from '../../../images/icon__COLOR_icon-main.svg';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+
 
 function NavTab({ isOpen, onClose }) {
+
+  const { pathname } = useLocation();
+
   return (
-    <article className={`navtab ${isOpen && 'navtab_opened'}`} aria-label="navtab">
+    <article className={`navtab ${isOpen && 'navtab_opened'}`} aria-label="navtab" onClick={onClose}>
       <section className="navtab__popup" aria-label="navtab">
         <button type="button" className="navtab__close" onClick={onClose}></button>
         <div className="navtab__navigate">
           <Link
             to="/"
-            className="navtab__navigate-link"
+            className={`navtab__navigate-link ${pathname === '/' ? 'navtab__navigate-link_active' : ''}`}
           >Главная</Link>
           <Link
             to="/movies"
-            className="navtab__navigate-link"
+            className={`navtab__navigate-link ${pathname === '/movies' ? 'navtab__navigate-link_active' : ''}`}
           >Фильмы</Link>
           <Link
             to="/saved-movies"
-            className="navtab__navigate-link"
+            className={`navtab__navigate-link ${pathname === '/saved-movies' ? 'navtab__navigate-link_active' : ''}`}
           >Сохранённые фильмы</Link>
         </div>
         <div className='navtab__account-links'>
